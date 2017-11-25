c57f89f865b6a1053f19b2ce9a280d1243d33539
yinm <yinm.cminor9@gmail.com>
2017-11-25 12:33:36 +0900

    Add method

--- ch1/sample.js
+++ ch1/sample.js
@@ -3,5 +3,8 @@ var cody = new Object();
 cody.living = true;
 cody.age = 33;
 cody.gender = 'male';
+cody.getGender = function() {
+  return cody.gender;
+};
 
-console.log(cody);
+console.log(cody.getGender());
